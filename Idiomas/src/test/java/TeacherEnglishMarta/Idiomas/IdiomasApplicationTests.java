f22cd2e21a6552beca138f3fa9e50fbce8b64820
Ricardo Souza <rickpcsouza@hotmail.com>
2024-11-19 11:20:09 -0300

    Add files via upload

---
+++ Idiomas/src/test/java/TeacherEnglishMarta/Idiomas/IdiomasApplicationTests.java
@@ -0,0 +1,13 @@
+package TeacherEnglishMarta.Idiomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdiomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
